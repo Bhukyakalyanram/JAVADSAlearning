755a3a57de62955df1ec7bedc21e11a070bdd08d
Bhukyakalyanram <Kalyanrambhukya54@gmail.com>
2025-06-01 12:25:39 +0530

    Recusion Lerning

--- Basicrecur.java
+++ Basicrecur.java
@@ -9,7 +9,26 @@ public class Basicrecur {
         PrintName(n, i + 1);
     }
 
+    public static void ascending(int n, int i) {
+        if (i > n) {
+            return;
+        }
+        System.out.println(i);
+        ascending(n, i + 1);
+    }
+
+    public static void desccending(int n) {
+        if (n < 0) {
+            return;
+        }
+        System.out.println(n);
+        desccending(n - 1);
+
+    }
+
     public static void main(String[] args) {
-        PrintName(5, 1);
+        // PrintName(5, 1);
+        // ascending(5, 1);
+        desccending(5);
     }
 }
