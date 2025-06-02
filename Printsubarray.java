67d8bb32dd965d54e48255620144637df3d2432f
Bhukyakalyanram <Kalyanrambhukya54@gmail.com>
2025-06-02 12:06:26 +0530

    HasharrayLerningbystriver

--- Printsubarray.java
+++ Printsubarray.java
@@ -14,8 +14,23 @@ public class Printsubarray {
         }
     }
 
+    public static int Maxsumaofsubarray(int arr[]) {
+        int maxsum = Integer.MIN_VALUE;
+        int currentsum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentsum = currentsum + arr[i];
+            if (currentsum < 0) {
+                currentsum = 0;
+            }
+            maxsum = Math.max(currentsum, maxsum);
+
+        }
+        return maxsum;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5,};
-        subarray(arr);
+        // subarray(arr);
+        System.out.println(Maxsumaofsubarray(arr));
     }
 }
