353dc8302684a30194833bd206ca74a55947f5f5
Bhukyakalyanram <Kalyanrambhukya54@gmail.com>
2025-06-04 20:02:59 +0530

    recurssion

---
+++ lastocc.java
@@ -0,0 +1,19 @@
+
+public class lastocc {
+
+    public static int last(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = last(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 3};
+        System.out.println(last(arr, 4, 0));
+    }
+}
---
+++ permutation.java
@@ -0,0 +1,23 @@
+
+public class permutation {
+
+    public static void permu(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+
+            String NewStr = str.substring(0, i) + str.substring(i + 1);
+
+            permu(NewStr, ans + curr);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        permu("abc", "");
+    }
+}
